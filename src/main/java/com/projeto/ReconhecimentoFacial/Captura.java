b7477480f56b0601a04743aa6880fb1fcbc258e1
Daniel Ribeiro <daniellopes03@hotmail.com>
2024-11-03 00:58:57 -0300

    Commit

---
+++ src/main/java/com/projeto/ReconhecimentoFacial/Captura.java
@@ -0,0 +1,7 @@
+package com.projeto.ReconhecimentoFacial;
+
+public class Captura {
+    public static void teste(){
+        System.out.println("Teste123");
+    }
+}
---
+++ src/test/java/com/projeto/ReconhecimentoFacial/ReconhecimentoFacialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ReconhecimentoFacial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconhecimentoFacialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

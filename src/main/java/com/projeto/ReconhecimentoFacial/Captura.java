d2e54a6b922499ea0eefa216a0e7ebcc1a6ae056
Daniel Ribeiro <daniellopes03@hotmail.com>
2024-11-06 21:14:59 -0300

    Commit

--- src/main/java/com/projeto/ReconhecimentoFacial/Captura.java
+++ src/main/java/com/projeto/ReconhecimentoFacial/Captura.java
@@ -1,7 +1,69 @@
 package com.projeto.ReconhecimentoFacial;
 
+import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+import static org.opencv.imgproc.Imgproc.COLOR_BGRA2GRAY;
+
 public class Captura {
-    public static void teste(){
-        System.out.println("Teste123");
+    public static void principal() throws FrameGrabber.Exception {
+
+        System.out.println("Digite seu id: ");
+        Scanner cadastro = new Scanner(System.in);
+        int idPessoa = cadastro.nextInt();
+        System.out.println("Seu id e " + idPessoa);
+
+        System.setProperty("java.awt.headless", "false");
+
+        OpenCVFrameConverter.ToMat converteMat = new OpenCVFrameConverter.ToMat();
+        OpenCVFrameGrabber camera = new OpenCVFrameGrabber(1);
+        camera.start();
+        CanvasFrame cFrame;
+
+        CascadeClassifier detectorFace = new CascadeClassifier("src/main/resources/haarcascade_frontalface_alt.xml");
+        Frame frameCapturado = null;
+        org.bytedeco.opencv.opencv_core.Mat imagemColorida = new org.bytedeco.opencv.opencv_core.Mat();
+        cFrame = new CanvasFrame("Preview");
+        int numeroAmostras = 100;
+        int amostra = 1;
+
+        while (camera.grab() != null) {
+            imagemColorida = converteMat.convert(camera.grab());
+            Mat imagemCinza = new Mat();
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+            RectVector facesDetectadas = new RectVector();
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150), new Size(500, 500));
+
+            for (int i = 0; i < facesDetectadas.size(); i++) {
+                Rect dadosFace = facesDetectadas.get(i);
+                rectangle(imagemColorida, dadosFace, new Scalar(0, 0, 255, 0));
+                Mat faceCapturada = new Mat(imagemCinza, dadosFace);
+                resize(faceCapturada, faceCapturada, new Size(160, 160));
+
+                if (amostra <= numeroAmostras) {
+                    imwrite("src/fotos/pessoa." + idPessoa + "." + amostra + ".jpg", faceCapturada);
+                    System.out.println("Foto" + amostra + " capturada \n");
+                    amostra++;
+                }
+                if (cFrame.isVisible()) {
+                    cFrame.showImage(converteMat.convert(imagemColorida));
+                }
+                if (!cFrame.isVisible() || amostra > numeroAmostras) {
+                    break;
+                }
+            }
+
+
+        }
+        cFrame.dispose();
+        camera.stop();
+        cadastro.close();
     }
 }
+
--- src/main/java/com/projeto/ReconhecimentoFacial/ReconhecimentoFacialApplication.java
+++ src/main/java/com/projeto/ReconhecimentoFacial/ReconhecimentoFacialApplication.java
@@ -1,11 +1,15 @@
 package com.projeto.ReconhecimentoFacial;
 
+import org.bytedeco.opencv.opencv_face.EigenFaceRecognizer;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.opencv.core.Core;
 
 @SpringBootApplication
 public class ReconhecimentoFacialApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReconhecimentoFacialApplication.class, args);
@@ -13,6 +17,6 @@ public class ReconhecimentoFacialApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Captura.teste();
+		Captura.principal();
 	}
 }

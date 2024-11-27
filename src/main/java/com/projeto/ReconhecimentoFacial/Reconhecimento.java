643d12e44e005b1381e88a0ffd1a54a3db9d400d
Daniel Ribeiro <daniellopes03@hotmail.com>
2024-11-27 08:57:08 -0300

    Commit

--- src/main/java/com/projeto/ReconhecimentoFacial/Reconhecimento.java
+++ src/main/java/com/projeto/ReconhecimentoFacial/Reconhecimento.java
@@ -1,36 +1,45 @@
 package com.projeto.ReconhecimentoFacial;
 
+import org.bytedeco.javacpp.DoublePointer;
+import org.bytedeco.javacpp.IntPointer;
 import org.bytedeco.javacv.*;
 import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_face.EigenFaceRecognizer;
+import org.bytedeco.opencv.opencv_face.FaceRecognizer;
+import org.bytedeco.opencv.opencv_face.FisherFaceRecognizer;
+import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
 import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
-
-import java.awt.event.KeyEvent;
-import java.util.Scanner;
 
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
 import static org.opencv.imgproc.Imgproc.COLOR_BGRA2GRAY;
 
-public class Captura {
-    public static void captura() throws FrameGrabber.Exception {
-
-        System.out.println("Digite seu id: ");
-        Scanner cadastro = new Scanner(System.in);
-        int idPessoa = cadastro.nextInt();
-        System.out.println("Seu id e " + idPessoa);
-
+public class Reconhecimento {
+    public static void reconhecimento() throws FrameGrabber.Exception {
         System.setProperty("java.awt.headless", "false");
 
         OpenCVFrameConverter.ToMat converteMat = new OpenCVFrameConverter.ToMat();
         OpenCVFrameGrabber camera = new OpenCVFrameGrabber(1);
+        String[] pessoas = {"","Daniel","Ivete"};
         camera.start();
         CanvasFrame cFrame;
         CascadeClassifier detectorFace = new CascadeClassifier("src/main/resources/haarcascade_frontalface_alt.xml");
+
+        //FaceRecognizer reconhecedor = EigenFaceRecognizer.create();
+        //reconhecedor.read("src/main/resources/classificadorEigenFaces.yml");
+        //reconhecedor.setThreshold(5000);
+
+        //FaceRecognizer reconhecedor = FisherFaceRecognizer.create();
+        //reconhecedor.read("src/main/resources/classificadorFisherFaces.yml");
+        //reconhecedor.setThreshold(2500);
+
+        FaceRecognizer reconhecedor = LBPHFaceRecognizer.create();
+        reconhecedor.read("src/main/resources/classificadorLbph.yml");
+        reconhecedor.setThreshold(90);
+
         Frame frameCapturado = null;
-        org.bytedeco.opencv.opencv_core.Mat imagemColorida = new org.bytedeco.opencv.opencv_core.Mat();
+        Mat imagemColorida = new Mat();
         cFrame = new CanvasFrame("Camera");
-        int numeroAmostras = 25;
-        int amostra = 1;
 
         while (camera.grab() != null) {
             imagemColorida = converteMat.convert(camera.grab());
@@ -45,11 +54,19 @@ public class Captura {
                 Mat faceCapturada = new Mat(imagemCinza, dadosFace);
                 resize(faceCapturada, faceCapturada, new Size(160, 160));
 
-                if (amostra <= numeroAmostras) {
-                    imwrite("src/fotos/pessoa." + idPessoa + "." + amostra + ".jpg", faceCapturada);
-                    System.out.println("Foto" + amostra + " capturada \n");
-                    amostra++;
+                IntPointer rotulo = new IntPointer(1);
+                DoublePointer confianca = new DoublePointer(1);
+                reconhecedor.predict(faceCapturada,rotulo,confianca);
+                int predicao = rotulo.get(0);
+                String nome;
+                if (predicao == -1){
+                    nome = "Desconhecido";
+                }else{
+                    nome = pessoas[predicao] + " - " + confianca.get(0);
                 }
+                int x = Math.max(dadosFace.tl().x() - 10,0);
+                int y = Math.max(dadosFace.tl().y() - 10,0);
+                putText(imagemColorida, nome, new Point(x,y),FONT_HERSHEY_PLAIN, 1.4, new Scalar(0,255,0,0));
             }
             if (cFrame.isVisible()) {
                 cFrame.showImage(converteMat.convert(imagemColorida));
@@ -59,7 +76,6 @@ public class Captura {
         }
         cFrame.dispose();
         camera.stop();
-        cadastro.close();
     }
 }
 
--- src/main/java/com/projeto/ReconhecimentoFacial/ReconhecimentoFacialApplication.java
+++ src/main/java/com/projeto/ReconhecimentoFacial/ReconhecimentoFacialApplication.java
@@ -17,6 +17,6 @@ public class ReconhecimentoFacialApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Treinamento.treinamento();
+		Reconhecimento.reconhecimento();
 	}
 }
